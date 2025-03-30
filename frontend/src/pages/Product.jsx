06e96cd98acff6f110e3f80e7ecc3688a11949e3
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2025-03-30 10:56:22 -0300

    ProductsTerminado

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -9,6 +9,7 @@ const Product = () => {
     const { products, currency } = useContext(ShopContext);
     const [productData, setProductData] = useState(false);
     const [image, setImage] = useState('')
+    const [size, setSize] = useState('')
 
     const fetchProductData = async () => {
 
@@ -59,12 +60,33 @@ const Product = () => {
                         <p>Select Size</p>
                         <div className='flex gap-2'>
                             {productData.sizes.map((item, index) => (
-                                <button key={index} >{item}</button>
+                                <button onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''} `} key={index} >{item}</button>
                             ))}
                         </div>
                     </div>
+                    <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                    <hr className='mt-8 sm:w-4/5' />
+                    <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                        <p>100% Original product.</p>
+                        <p>Cash on delivery is available on this product.</p>
+                        <p>Easy return and exchange policy within 7 days.</p>
+                    </div>
                 </div>
             </div>
+            {/* Descripcion y review section  */}
+            <div className='mt-20'>
+                <div className='flex'>
+                    <b className='border px-5 py-3 text-sm'>Description</b>
+                    <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+                </div>
+                <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis explicabo possimus rem blanditiis ut ea ipsam nostrum magni, perferendis reiciendis?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officia, placeat saepe, praesentium similique iusto nesciunt asperiores magnam dolores non repellat.</p>
+                </div>
+            </div>
+            {/* Mostrar producto relacionado */}
+
+
         </div>
     ) : <div className='opacity-0'> </div>
 }
