2fc77903cdd63a5febd633026e5b603a744df0d3
Marcos-Echeverria <marcosalbertoecheverria@gmail.com>
2025-03-31 17:21:44 -0300

    ProductRelated

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import ProductItem from './ProductItem'
+import Title from './Title.jsx'
 
 const RelatedProducts = ({ category, subCategory }) => {
     const { products } = useContext(ShopContext)
@@ -11,11 +13,20 @@ const RelatedProducts = ({ category, subCategory }) => {
             productsCopy = productsCopy.filter((item) => category === item.category);
             productsCopy = productsCopy.filter((item) => subCategory === item.subCategory);
 
-            console.log(productsCopy.slice(0, 50));
+            setRelated(productsCopy.slice(0, 50));
         }
     }, [products])
     return (
-        <div></div>
+        <div className='my-24'>
+            <div className='text-center text-3xl py-2'>
+                <Title text1={'RELATED'} text2={'PRODUCTS'} />
+            </div>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {related.map((item, index) => (
+                    <ProductItem key={index} id={item._id} name={item.name} price={item.price} image={item.image} />
+                ))}
+            </div>
+        </div>
     )
 }
 
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -6,8 +6,25 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
     const currency = '$';
     const delivery_fee = 10;
-    const [search, setSearch] = useState('')
-    const [showSearch, setShowSearch] = useState(false)
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemId, size) => {
+        let cartData = structuredCloner(cartItems)
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            } else {
+                cartData[itemId][size] = 1;
+            }
+        } else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+
+    }
 
     const value = {
         products,
